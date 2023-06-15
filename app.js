c708cc612ebdb91e604c555571ea25ea09211af7
oceanicfish <oceanicfish@gmail.com>
2023-06-15 11:31:09 +0900

    fixed app.js comment out log

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var http = require('http');
 var sockjs = require('sockjs');
 
+const DEBUG = false;
+
 var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
 var clients = [];
 
 echo.on('connection', function(conn) {
     clients[conn.id] = conn;
-    console.log('[wsserver] Client ' + conn.id + ' connected.');
+    DEBUG && console.log('[wsserver] Client ' + conn.id + ' connected.');
     conn.write('[wsserver] Client ' + conn.id + ' connected.');
     conn.on('data', function(message) {
-        console.log('[wsserver] Message ' + message);
+        DEBUG && console.log('[wsserver] Message ' + message);
         if (message == 'console_cmd_play') {
             for(key in clients) {
                 if(clients.hasOwnProperty(key)) {
-                    console.log('[wsserver] Sending message [' + message + '] to ' + clients[key]);
+                    DEBUG && console.log('[wsserver] Sending message [' + message + '] to ' + clients[key]);
                     clients[key].write('cmd_play');
                 }
             }
