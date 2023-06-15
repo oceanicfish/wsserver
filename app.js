57251ef87fb5300662023f3422800b6345668823
oceanicfish <oceanicfish@gmail.com>
2023-06-15 10:29:55 +0900

    added sockjs

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var http = require('http');
+var sockjs = require('sockjs');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
+var clients = [];
+
+echo.on('connection', function(conn) {
+    clients[conn.id] = conn;
+    console.log('[wsserver] Client ' + conn.id + ' connected.');
+    conn.write('[wsserver] Client ' + conn.id + ' connected.');
+    conn.on('data', function(message) {
+        if (message == 'console_cmd_play') {
+            for(key in clients) {
+                if(clients.hasOwnProperty(key)) {
+                    clients[key].write('cmd_play');
+                }
+            }
+        }
+    });
+    conn.on('close', function() {
+        delete clients[conn.id];
+    });
+});
+
+var server = http.createServer();
+echo.installHandlers(server, {prefix:'/tv'});
+server.listen(9999, '0.0.0.0');
