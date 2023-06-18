310597a78432d37efb1fccfcf06fbf739811c0b0
oceanicfish <oceanicfish@gmail.com>
2023-06-18 17:53:37 +0900

    Fixed to https

--- app.js
+++ app.js
@@ -2,20 +2,20 @@
 // import { readFileSync } from 'fs';
 // import { WebSocketServer } from 'ws';
 
-// var https = require('https');
-var http = require('http');
+var https = require('https');
+// var http = require('http');
 var fs = require('fs');
 var ws = require('ws');
 
-// const server = https.createServer({
-//   cert: fs.readFileSync('/etc/letsencrypt/live/fp276e134c.tkye313.ap.nuro.jp/fullchain.pem'),
-//   key: fs.readFileSync('/etc/letsencrypt/live/fp276e134c.tkye313.ap.nuro.jp/privkey.pem')
-// });
+const server = https.createServer({
+  cert: fs.readFileSync('/etc/pki/tls/certs/apache-selfsigned.crt'),
+  key: fs.readFileSync('/etc/pki/tls/private/apache-selfsigned.key')
+});
 // const server = https.createServer({
 //   cert: fs.readFileSync('keys/fullchain.pem'),
 //   key: fs.readFileSync('keys/privkey.pem')
 // });
-const server = http.createServer();
+// const server = http.createServer();
 const wss = new ws.WebSocketServer({ server });
 
 wss.on('connection', function connection(ws) {
